0a08898d40c1bc12e72ab4c3cfbc429184920c4f
Sriram Sundarraj <sriram@retool.com>
2024-12-02 13:44:12 +0530

    day01 part01 awk

--- 2024/01/solution.sh
+++ 2024/01/solution.sh
@@ -1,20 +1,13 @@
-#!/bin/bash
-
-# todo awk
-col1=($(cut -d ' ' -f1 input.txt | sort))
-col2=($(cut -d ' ' -f4 input.txt | sort))
-
-function diff {
-        if [ $1 -lt $2 ]; then
-                echo $(($2 - $1))
-        else
-                echo $(($1 - $2))
-        fi
-}
-
-sum=0
-for i in $(seq 0 $((${#col1[@]} - 1))); do
-        ((sum += $(diff ${col1[$i]} ${col2[$i]})))
-done
-
-echo $sum
+gawk '{
+  xs[NR-1] = $1
+  ys[NR-1] = $2
+} END {
+  asort(xs)
+  asort(ys)
+  for (i in xs) {
+    diff = xs[i] - ys[i]
+    diff = diff < 0 ? -diff : diff
+    sum += diff
+  }
+  print sum
+}' $1
