c269fa9820cd33307a478b00f9173c71a67dd00c
Sriram Sundarraj <sriram@retool.com>
2024-12-11 12:26:47 +0530

    day06 part01

---
+++ 2024/06/solution01.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+gawk -F '' '{
+        for (i=1; i<=NF; ++i) {
+                if ($i == "#") {
+                        walls[NR][i] = 1
+                }
+                if ($i == "^") {
+                        gi = NR
+                        gj = i
+                        di = -1
+                        dj = 0
+                }
+                if ($i == 'v') {
+                        gi = NR
+                        gj = i
+                        di = 1
+                        dj = 0
+                }
+        }
+} END {
+        print gi gj di dj
+       while (gi >= 1 && gj >= 1 && gi <= NR && gj <= NF) {
+               if (seen[gi][gj] != 1)
+                        ++steps
+               seen[gi][gj] = 1
+               
+                if (walls[gi+di][gj+dj] == 1) {
+                       if (di == 0) {
+                               di = dj
+                               dj = 0
+                       } else if (dj == 0) {
+                              dj = di * -1
+                               di = 0
+                       } 
+                }
+                gi += di
+                gj += dj
+                print gi " and " gj " and " di " and " dj
+        }
+        print steps
+}' $1
